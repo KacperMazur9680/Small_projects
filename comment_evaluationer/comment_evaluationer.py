5d51a376e942629f76cbcda238dab55a4d8983ae
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-05-09 13:20:04 +0200

    Added with git

--- comment_evaluationer/comment_evaluationer.py
+++ comment_evaluationer/comment_evaluationer.py
@@ -1,51 +1,49 @@
-import glob
-POSITIVE = glob.glob("Small_projects/comment_evaluationer/comments/pos/*.txt")
-NEGATIVE = glob.glob("Small_projects/comment_evaluationer/comments/neg/*.txt")
-PUNCTUATION = ",.?!@#$%^&()"
+import glob 
+POSITIVE = glob.glob("Small_projects/comment_evaluationer/comments/pos/*.txt")  # Path with the positive comments
+NEGATIVE = glob.glob("Small_projects/comment_evaluationer/comments/neg/*.txt")  # Path with the negative comments
+PUNCTUATION = "-+!@#$%^&*(),.?/"
 
-positive_comments = []
-negative_comments = []
-
-# for each file in the pos/neg directory, replace html's "enter" and any punctuations with space,
-# make every word lower case and convert every file to a list that we append to an external list.
+# For each file in the pos/neg directory, replace html's "enter" and any punctuations with a space,
+# make every word lowercase, convert every file to a list of words,
+# convert made list to a set to avoid repetitions, add each word to the dictionary,
+# Note: if said word is already in the dictionary its value is increaced by one
+positive_dict = {}
 for file in POSITIVE:
-    with open(file, encoding="utf8") as f:
-        content = f.read()
-    content = content.replace("<br />", " ")
+    with open(file, encoding="utf8") as stream:
+        content = stream.read()
+
+    content = content.replace("<br /><br />", " ")
     for punc in PUNCTUATION:
         content = content.replace(punc, " ")
+
     content = content.lower().split()
-    positive_comments.append(content)
+    for word in set(content):
+        positive_dict[word] = positive_dict.get(word, 0) + 1
+
+negative_dict = {}
 
 for file in NEGATIVE:
-    with open(file, encoding="utf8") as f:
-        content = f.read()
-    content = content.replace("<br />", " ")
+    with open(file, encoding="utf8") as stream:
+        content = stream.read()
+
+    content = content.replace("<br /><br />", " ")
     for punc in PUNCTUATION:
         content = content.replace(punc, " ")
-    content = content.lower().split()
-    negative_comments.append(content)
 
-# new comment prompt.
-new_comment = input("Enter you comment: ")
-words = new_comment.split()
+    content = content.lower().split()
+    for word in set(content):
+        negative_dict[word] = negative_dict.get(word, 0) + 1
+
+# Prompt the user for a comment
+new_comment = input("Enter your comment: ")
+words = new_comment.lower().split()
 sentiment_sum = 0
 
-# going through every word.
-for word in words:
-    positive = 0  
-    negative = 0  
+# Math behind each words sentiment
+for word in words: 
+    positive = positive_dict.get(word, 0)
+    negative = negative_dict.get(word, 0)
 
-# checking in how many listed comments (pos and neg) a certain word appeared.
-    for comment in positive_comments:
-        if word.lower() in comment:
-            positive += 1
-
-    for comment in negative_comments:
-        if word.lower() in comment:
-            negative += 1
-
-# math behind each words sentiment.
     all_ = positive + negative
     if all_ != 0:
         word_sentiment = (positive-negative)/all_
@@ -57,7 +55,7 @@ for word in words:
 
 sentiment = sentiment_sum/len(words)
 
-# informing what kind of comment was added. 
+# Informing what kind of comment was added
 if sentiment > 0:
     label = "positive"
 elif sentiment < 0:
@@ -65,4 +63,4 @@ elif sentiment < 0:
 else:
     label = "neutral"
 
-print(f"The sentense in {label} and it has a sentiment {sentiment}")
+print(f"The added comment is {label} and it has a {sentiment} sentiment.")
