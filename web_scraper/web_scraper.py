a1266f371e1cbfcded336753e3411f639f66b2ea
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-09-05 19:34:52 +0200

    Added a new functionality

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -1,27 +1,56 @@
 import requests
+from bs4 import BeautifulSoup
+import sys
+
+
+def link_check(url):
+    if "https://www.imdb.com/title/" in url:
+        check = True
+    else:
+        check = False
+    return check
+
+
+def description(soup):
+    try:
+        descrp = soup.find('span', {'data-testid': 'plot-l'})
+    except KeyError:
+        print("Invalid movie page!")
+        sys.exit(2)
+    else:
+        return descrp.text
+
+
+def movie_title(soup):
+    try:
+        title = soup.find('h1')
+    except KeyError:
+        print("Invalid movie page!")
+        sys.exit(2)
+    else:
+        return title.text
 
 
 def return_content(url):
-    r = requests.get(url)
-    if r.status_code == 200:
-        raw_content = r.json()
-        try:
-            raw_content["content"]
-            
-        except KeyError:
-            content = "Invalid quote resource!"
-        else:
-            content = raw_content["content"]
-
+    if link_check(url):
+        r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
     else:
-        content = "Invalid quote resource!"
+        print("Invalid movie page!")
+        sys.exit(1)
 
-    return content
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        m_title = movie_title(soup)
+        descr = description(soup)
+        ingdr = {"title": m_title, "description": descr}
+        print(ingdr)
+    else:
+        print("Invalid quote resource!")
 
 
 def main():
     url = input("Input the URL:\n")
-    print(return_content(url))
+    return_content(url)
 
 
 if __name__ == "__main__":
