38528556eb8011f4e5a7b97f793e38d676ec8e74
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-09-08 22:37:24 +0200

    The user can now choose how many pages to scrap and for what kind of article

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -1,45 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 import string
+import os
 
 
 
-def return_content(url):
+def return_content(num_pages, article_type):
+    for num in range(1, num_pages+1):
 
-    r = requests.get(url)
+        url = f"https://www.nature.com/nature/articles?sort=PubDate&year=2020&page={num}"
 
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.content, 'html.parser')
-        news = soup.find_all("span", string="News")
-        for new in news:
-            article = new.find_previous("a")
-            article_link = "https://www.nature.com" + article.get("href")
-            s = requests.get(article_link)
-            sp = BeautifulSoup(s.content, "html.parser")
+        r = requests.get(url)
 
-            title_raw = sp.find("h1")
-            content_raw = sp.find(id = "content")
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            articles = soup.find_all("span", string=article_type)
+            for new in articles:
+                article = new.find_previous("a") 
+                article_link = "https://www.nature.com" + article.get("href")
+                s = requests.get(article_link)
+                sp = BeautifulSoup(s.content, "html.parser")
 
-            title = title_raw.text
-            content = content_raw.text
+                title_raw = sp.find("h1")
+                content_raw = sp.find("div", {"class":"c-article-body u-clearfix"})
+
+                title = title_raw.text
+                content = content_raw.text
+                
+                for char in title:
+                    if char in string.punctuation:
+                        title = title.replace(char, "")
             
-            for char in title:
-                if char in string.punctuation or char == "’" :
-                    title = title.replace(char, "")
-           
-                if char in string.whitespace:
-                    title = title.replace(char, "_")
+                    if char in string.whitespace:
+                        title = title.replace(char, "_")
 
-            with open("web_scraper/news/" + title + ".txt", "wb") as stream:
-                stream.write(content.encode('UTF-8'))
+                try:
+                    os.mkdir(f"web_scraper/Page_{num}")
 
-    else:
-        print(f"The URL returned {r.status_code}!")
+                    with open(f"web_scraper/Page_{num}/" + title + ".txt", "wb") as stream:
+                        stream.write(content.encode('UTF-8'))
+                except FileExistsError:
+                    with open(f"web_scraper/Page_{num}/" + title + ".txt", "ab") as stream:
+                        stream.write(content.encode('UTF-8'))
 
+        else:
+            print(f"The URL returned {r.status_code}!")
+    
+    print("Done!")
 
 def main():
-    url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
-    return_content(url)
+    pages = int(input("How many pages to search: "))
+    article_type = input("What type of article are you looking for: ")
+    return_content(pages, article_type)
 
 
 if __name__ == "__main__":
