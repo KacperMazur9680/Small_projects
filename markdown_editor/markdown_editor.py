34ebd52bbad576fa83c4d2f7b664d534e1d79b4c
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-09-24 22:19:25 +0200

    Added the basis

---
+++ markdown_editor/markdown_editor.py
@@ -0,0 +1,77 @@
+FORMATTERS = ["plain", "bold", "italic", "header", "link", "inline-code", "ordered-list", "unordered-list", "new-line"]
+
+
+def header():
+    while True:
+        lvl = int(input("Level: "))
+        if lvl <= 0 or lvl > 6:
+            print("The level should be within the range of 1 to 6")
+        else:
+            break
+
+    text = input("Text: ")
+    if len(f_texts) == 0:
+        return "#" * lvl + " " + text + "\n"
+    else:
+        return "\n" + "#" * lvl + " " + text + "\n"
+
+def plain():
+    text = input("Text: ")
+    return text
+
+def bold():
+    text = input("Text: ")
+    return f"**{text}**"
+
+def italic():
+    text = input("Text: ")
+    return f"*{text}*"
+
+def inline():
+    text = input("Text: ")
+    return f"`{text}`"
+
+def link():
+    label = input("Label: ")
+    url = input("URL: ")
+    return f"[{label}]({url})"
+
+f_texts = []
+counter = 0
+
+while True:
+    if counter == 1:
+        format = input("\nChoose a formatter: ")
+    else:
+        format = input("Choose a formatter: ")
+
+    if format == "!help":
+        print("""Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line
+Special commands: !help !done""")
+        continue
+    
+    if format == "!done":
+        break
+        
+    if format in FORMATTERS:
+        if format == "header":
+            f_texts.append(header())
+        elif format == "plain":
+            f_texts.append(plain())
+        elif format == "bold": 
+            f_texts.append(bold())
+        elif format == "italic":
+            f_texts.append(italic())
+        elif format == "inline-code":
+            f_texts.append(inline())
+        elif format == "link":
+            f_texts.append(link())
+        else:
+            f_texts.append("\n")
+        for text in f_texts:
+            print(text, end="")
+    else:
+        print("Unknown formatting type or command")
+        pass
+    
+    counter = 1
