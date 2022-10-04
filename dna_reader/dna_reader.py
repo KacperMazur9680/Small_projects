9fff8c81590bedfe051a0a3b5a35258a3075985b
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-10-04 21:22:57 +0200

    Determines the best archive

--- dna_reader/dna_reader.py
+++ dna_reader/dna_reader.py
@@ -1,39 +1,75 @@
-file = "./dna_reader/dna_data/dna.fastq"
-x = {}
-y = []
-strands = []
-gc = []
-
-with open(file, "r") as stream:
-    content = stream.read()
-
-dnas = ["@SRR" + info for info in content.split("@SRR") if info]
-reads = len(dnas)
-
-for info in dnas:
-    info_f = info.split("\n")
-    read_ = info_f[1]
-    length = len(read_)
-    x[length] = x.get(length, 0) + 1
-    y.append(length)
-    strands.append(read_)
-
-print(f"Reads in the file = {reads}")
-for key, value in x.items():
-    print(f"    with length {key} = {value}")
-
-print(f"Reads sequence average length = {int(round(sum(y)/reads, 0))}")
-
-print()
-print(f"Repeats = {len(strands) - len(set(strands))}")
-print()
+import gzip
 
 
-for strand in strands:
-    gc_content = 0
-    for form in strand:
-        if form == "G" or form == "C":
-            gc_content += 1
-    gc.append(round(gc_content / len(strand) * 100, 2))
+file1 = "data1.gz"
+file2 = "data2.gz"
+file3 = "data3.gz"
 
-print(f"GC content average = {round(sum(gc) / reads, 2)}%")
+files = [file1, file2, file3]
+
+files_info = {}
+
+for file in files:
+    with gzip.open(file, "rt") as stream:
+        content = stream.read()
+
+    lengths_counter = {}
+    lengths = []
+    strands = []
+    gc = []
+    ns = []
+    n = 0
+    undef_nucl = 0
+
+    dnas = ["@SRR" + info for info in content.split("@SRR") if info]
+    reads = len(dnas)
+
+    for info in dnas:
+        info_f = info.split("\n")
+        read_ = info_f[1]
+        length = len(read_)
+        lengths_counter[length] = lengths_counter.get(length, 0) + 1
+        lengths.append(length)
+        strands.append(read_)
+
+    avg_len = int(round(sum(lengths)/reads, 0))
+
+    reps = len(strands) - len(set(strands))
+
+    for strand in strands:
+        undef_nucl = 0
+        if "N" in strand:
+            n += 1
+            for nucl in strand:
+                if nucl == "N":
+                    undef_nucl += 1
+            ns.append(undef_nucl * 100 / len(strand))
+
+    for strand in strands:
+        gc_content = 0
+        for form in strand:
+            if form == "G" or form == "C":
+                gc_content += 1
+        gc.append(round(gc_content / len(strand) * 100, 2))
+
+    avg_gc = f"{round(sum(gc) / reads, 2)}%"
+    ns_per_read = f"{round(sum(ns) / reads , 2)}%"
+
+    files_info.update({file: [reads, avg_len, reps, n, avg_gc, ns_per_read]})
+
+files_reps_ns = []
+for file, info in files_info.items():
+    if info[2] == 0:
+        files_reps_ns.append(info[3])
+
+best_file = min(files_reps_ns)
+
+for key, value in files_info.items():
+    if best_file == value[3]:
+        print(key)
+        print(f"Reads in the file = {value[0]}")
+        print(f"Reads sequence average length = {value[1]}\n")
+        print(f"Repeats = {value[2]}")
+        print(f"Reads with Ns = {value[3]}\n")
+        print(f"GC content average = {value[4]}")
+        print(f"Ns per read sequence = {value[5]}")
