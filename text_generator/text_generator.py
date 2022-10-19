c5be3f9eb6a26c972c32a9dcd7dd3463b19a202a
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-10-19 23:35:08 +0200

    Generates pseudo-sentences, needs refactoring

--- text_generator/text_generator.py
+++ text_generator/text_generator.py
@@ -4,6 +4,7 @@ import nltk
 import random
 
 FILENAME = "./text_generator/data/corpus.txt"
+PUNC = "?!,."
 
 
 with open(FILENAME, "r", encoding="utf-8") as f:
@@ -16,14 +17,20 @@ head_tails = {}
 for head, tail in bigrams:
     head_tails.setdefault(head, []).append(tail)
 
-words_in_sentence = 10
+words_in_sentence = 4
 sentences = 10
+
 for s in range(sentences):
     text = ""
-    head = random.choice(list(head_tails.keys()))
-    for w in range(words_in_sentence):
+    w = 0
+    while True:
+        head = random.choice(list(head_tails.keys()))
+        if head[0].isupper() and head[-1] not in PUNC:
+            text += f"{head} "
+            break
+    
+    while w < words_in_sentence:
         reps = dict(Counter(head_tails[head]))
-
         most_propable = []
 
         for key, value in reps.items():
@@ -32,5 +39,22 @@ for s in range(sentences):
 
         head = random.choice(most_propable)
         text += f"{head} "
+
+        w += 1
     
+    if text[-2] not in PUNC:
+        while True:
+            reps = dict(Counter(head_tails[head]))
+            most_propable = []
+
+            for key, value in reps.items():
+                if value == max(list(reps.values())):
+                    most_propable.append(key)
+
+            head = random.choice(most_propable)
+            text += f"{head} "
+
+            if text[-2] in PUNC:
+                break
+
     print(text)
