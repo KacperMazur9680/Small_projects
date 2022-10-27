53fae1d8736d858323bdcb6aae7fccb2a032f81d
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-10-27 16:33:03 +0200

    Added some test comparisons

---
+++ regex_engine/test_regex.py
@@ -0,0 +1,23 @@
+import unittest
+from regex_engine import Regex_Engine
+
+class Test_Regex(unittest.TestCase):
+
+    def test_output(self):
+        self.assertEqual(Regex_Engine("^app|apple").compare(), True)
+        self.assertEqual(Regex_Engine("le$|apple").compare(), True)
+        self.assertEqual(Regex_Engine("^a|apple").compare(), True)
+        self.assertEqual(Regex_Engine(".$|apple").compare(), True)
+        self.assertEqual(Regex_Engine("apple$|tasty apple").compare(), True)
+        self.assertEqual(Regex_Engine("^apple|apple pie").compare(), True)
+        self.assertEqual(Regex_Engine("^apple$|apple").compare(), True)
+        self.assertEqual(Regex_Engine("^apple$|tasty apple").compare(), False)
+        self.assertEqual(Regex_Engine("^apple$|apple pie").compare(), False)
+        self.assertEqual(Regex_Engine("app$|apple").compare(), False)
+        self.assertEqual(Regex_Engine("^le|apple").compare(), False)
+        self.assertEqual(Regex_Engine("a|a").compare(), True)
+        self.assertEqual(Regex_Engine(".h|ho").compare(), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
