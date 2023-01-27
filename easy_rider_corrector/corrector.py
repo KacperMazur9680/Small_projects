dd633f6a393627234f2c076bb2228b94aa0b1498
Kacper Mazur <mazur.kacper9680@gmail.com>
2023-01-27 20:47:21 +0100

    Added format (from documentation) errors to the count

--- easy_rider_corrector/corrector.py
+++ easy_rider_corrector/corrector.py
@@ -1,6 +1,14 @@
 import json
+import re
 
-json_input = input("Input data: ")
+json_input = input()
+
+error_dic = {"bus_id": 0,
+             "stop_id": 0,
+             "stop_name": 0,
+             "next_stop": 0,
+             "stop_type": 0,
+             "a_time": 0}
 
 bi_e = 0
 si_e = 0
@@ -11,33 +19,74 @@ at_e = 0
 
 str_to_dict = json.loads(json_input)
 
+# Type checking:
 for dic in str_to_dict:
     for el in dic.items():
         if el[0] == "bus_id":
             if type(el[1]) is not int or el[1] == "":
-                bi_e += 1
+                error_dic["bus_id"] += 1
+
         if el[0] ==  "stop_id":
             if type(el[1]) is not int or el[1] == "":
-                si_e += 1
+                error_dic["stop_id"] += 1
+
         if el[0] == "stop_name":
             if type(el[1]) is not str or el[1] == "":
-                sn_e += 1
+                error_dic["stop_name"] += 1
+
         if el[0] == "next_stop":
             if type(el[1]) is not int or el[1] == "":
-                ns_e += 1
+                error_dic["next_stop"] += 1
+
         if el[0] == "stop_type":
             if type(el[1]) is not str:
-                st_e += 1
+                error_dic["stop_type"] += 1
             elif len(el[1])>1:
-                st_e += 1
+                error_dic["stop_type"] += 1
+
         if el[0] == "a_time":
             if type(el[1]) is not str or el[1] == "":
-                at_e += 1
+                error_dic["a_time"] += 1
 
-print("Type and required field validation:", bi_e + si_e + sn_e + ns_e + st_e + at_e, "errors")
-print("bus_id", bi_e)
-print("stop_id", si_e)
-print("stop_name", sn_e)
-print("next_stop", ns_e)
-print("stop_type", st_e)
-print("a_time", at_e)
+# Format checking:
+for dic in str_to_dict:
+    for el in dic.items():
+        if el[0] == "stop_name":
+            try:
+                pattern = "([A-Z]\w+ ?)+ (Road|Avenue|Boulevard|Street)$"
+                string = el[1]
+
+                result = re.match(pattern, string)
+                if result is None:
+                    error_dic["stop_name"] += 1
+                    print(el[1])
+            except TypeError:
+                pass
+ 
+        if el[0] == "stop_type":
+            try:
+                pattern = "S|O|F"
+                string = el[1]
+                result = re.match(pattern, string)
+                
+                if result is None:
+                    error_dic["stop_type"] += 1
+                if el[1] == "":
+                    error_dic["stop_type"] -= 1
+            except TypeError:
+                pass
+        
+        if el[0] == "a_time":
+            try:
+                pattern = "^(0[1-9]|1[0-9]|2[0-3]):[0-5][0-9]$"
+                string = el[1]
+                result = re.match(pattern, string)
+                if result is None:
+                    error_dic["a_time"] += 1
+            except TypeError:
+                pass
+
+print("Type and required field validation:", sum(error_dic.values()), "errors")
+
+for k,v in error_dic.items():
+    print(k, v)
