68c789a01debc36123b998c1b9153ba1a68acf90
Kacper Mazur <mazur.kacper9680@gmail.com>
2023-02-07 12:22:05 +0100

    Dictionary-based brute force

--- password_hacker/hacker.py
+++ password_hacker/hacker.py
@@ -3,11 +3,11 @@ import socket
 import itertools
 import os
 
-def checker(data, client_socket):
+def checker(data, client_socket, bufor=1024):
     for p in data:
         bp = p.encode()
         client_socket.send(bp)
-        response = client_socket.recv(1024)
+        response = client_socket.recv(bufor)
         response = response.decode()
         if response == "Connection success!":
             return p
@@ -29,6 +29,23 @@ def brute_force(client_socket):
             return check
         rep += 1
 
+def dict_brute_force_possibl():
+    pass_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "password.txt")
+    with open(pass_path, "r") as stream:
+        datas = stream.readlines()
+
+    datas = [data.replace("\n", "") for data in datas]
+    for data in datas:
+        word_low_up = itertools.product(*([char.lower(), char.upper()] for char in data))
+        for word in word_low_up:
+            word = "".join(word)
+            yield word
+
+def dict_brute_force(client_socket):
+     data = dict_brute_force_possibl()
+     check = checker(data, client_socket, bufor=10240)
+     if check:
+         return check
 
 def connect(client_socket):
     hostname = sys.argv[1]
@@ -40,8 +57,8 @@ def connect(client_socket):
 def main():
     with socket.socket() as client_socket:
         connect(client_socket)
-        print(brute_force(client_socket))
+        # print(brute_force(client_socket))
+        print(dict_brute_force(client_socket))
 
 if __name__ == "__main__":
     main()
-
