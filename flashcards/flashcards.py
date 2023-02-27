c493c97f1f80b6b0f94ec51266d5faaefcb7f262
Kacper Mazur <mazur.kacper9680@gmail.com>
2023-02-27 21:16:55 +0100

    Added wrong answer counting, hardest card to learn based on answers and updated the class to work with the new features

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -1,6 +1,10 @@
+from io import StringIO
+
 class Flashcards:
     def __init__(self) -> None:
         self.base = {}
+        self.wrong_ans_base = {}
+        self.memory_file = StringIO()
 
     def add(self) -> None:
         
@@ -28,6 +32,8 @@ class Flashcards:
         check_dup(self.defin)
 
         self.base.update({self.term:self.defin})
+        self.wrong_ans_base.update({self.term:0})
+
         print(f'The pair ("{self.term}":"{self.defin}") has been added.\n')
 
     def remove(self) -> None:
@@ -51,7 +57,10 @@ class Flashcards:
         num = 0
         for card in cards:
             card = card.replace("\n","")
-            self.base.update(eval(card))
+            card, wrong = card.rsplit(" ", 1)
+            card = eval(card)
+            self.base.update(card)
+            self.wrong_ans_base.update({list(card.keys())[0]: int(wrong)})
             num += 1
 
         print(f"{num} cards have been loaded.\n")
@@ -60,9 +69,10 @@ class Flashcards:
         file = input("File name:\n")
         num = 0
         
-        with open(file, "w") as stream:
+        with open("./flashcards/" + file, "w") as stream:
             for card in self.base.items():
-                card = str(card).replace("(","{").replace(")","}").replace(",",":") + "\n"
+                term, _ = card
+                card = str(card).replace("(","{").replace(")","}").replace(",",":") + f" {self.wrong_ans_base[term]}\n"
                 stream.write(card)
                 num += 1
         
@@ -83,30 +93,49 @@ class Flashcards:
                             for key, val in self.base.items():
                                 if val == ans:
                                     print(f'Wrong. The right answer is "{defin}", but your definition is correct for "{key}".')
+                                    self.wrong_ans_base[term] += 1
                         else:
                             print(f'Wrong. The right answer is "{defin}".')
+                            self.wrong_ans_base[term] += 1
+
                     track += 1
 
                 else:
                     break
 
     def log(self) -> None:
+        filename = input("File name:\n")
+        
+
         """ ask the user where to save the log with the message 'File name:',
         save all the lines that have been input in/output to the console to the file,
         and print the message 'The log has been saved'. Don't clear the log after saving it to the file."""
+        """
+        from datetime import datetime
+
+        now = datetime.now()
+
+        current_time = now.strftime("%H:%M:%S")
+        print("Current Time =", current_time)"""
         pass
     
     def hardest(self) -> None:
-        """print a string that contains the term of the card with the highest number of wrong answers, 
-        for example, 'The hardest card is "term". You have N errors answering it.' 
-        If there are several cards with the highest number of wrong answers, 
-        print all of the terms: 'The hardest cards are "term_1", "term_2"'. 
-        If there are no cards with errors in the user's answers, print the message 'There are no cards with errors.'"""
-        pass
+        wrong_ans = dict(sorted(self.wrong_ans_base.items(), reverse=True, key=lambda item: item[1]))
+        most_wrong_val = max(list(wrong_ans.values()))
+
+        list_of_hardest = ['"'+term+'"' for term in wrong_ans.keys() if wrong_ans[term] == most_wrong_val]
+
+        if len(list_of_hardest) == 1:
+            print(f'The hardest card is {list_of_hardest[0]}. You have {most_wrong_val} errors answering it.\n')
+        elif len(list_of_hardest) > 1:
+            print(f'The hardest cards are {", ".join(list_of_hardest)}. You have {most_wrong_val} errors answering them.\n')
+        else:
+            print("There are no cards with errors.\n")
     
     def reset_stats(self) -> None:
-        """set the count of mistakes to 0 for all the cards and output the message 'Card statistics have been reset.'"""
-        pass
+        for card in self.wrong_ans_base.keys():
+            self.wrong_ans_base[card] = 0
+        print('Card statistics have been reset.\n')
 
     def run(self, action: str) -> None:   
         action = action.lower()
